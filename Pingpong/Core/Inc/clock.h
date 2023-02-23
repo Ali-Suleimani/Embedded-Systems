579fb57e6e8fe8296ebd7c99b5d9b4883132ff7f
Ali Suleimani <125152257+Ali-Suleimani@users.noreply.github.com>
2023-02-23 22:11:10 +0000

    Add files via upload

---
+++ Pingpong/Core/Inc/clock.h
@@ -0,0 +1,13 @@
+/*
+ * clock.h
+ *
+ *  Created on: 11 nov. 2022
+ *      Author: Ali Suleimani
+ */
+
+#ifndef INC_CLOCK_H_
+#define INC_CLOCK_H_
+
+void SystemClock_Config(void);
+
+#endif /* INC_CLOCK_H_ */
---
+++ Pingpong/Core/Inc/test.h
@@ -0,0 +1,16 @@
+/*
+ * Test.h
+ *
+ *  Created on: 9 nov. 2022
+ *      Author: Ali Suleimani
+ */
+
+#ifndef INC_TEST_H_
+#define INC_TEST_H_
+
+void Test_program(void);
+void Test_Led(void);
+void Test_Show_points();
+void Test_buttons(void);
+
+#endif /* INC_TEST_H_ */
